14ad19bb45aaf212e81a22155575f4e04b613bc2
OZGE <ozgesurer2019@u.northwestern.edu>
2021-07-09 16:39:21 -0500

    Updating version numbers

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name="surmise",
-    version="0.1.0",
+    version="0.1.1",
     author="Matthew Plumlee, Özge Sürer, Stefan M. Wild",
     author_email="ozgesurer2019@u.northwestern.edu",
     description="A modular interface for surrogate models and tools",
