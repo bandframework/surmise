23aa45f34799bda97afff09e73d27b099a42e4c1
Moses Chan <mosesyhc@u.northwestern.edu>
2022-07-14 15:46:58 -0500

    timescale test

---
+++ scratch/timescaling_gp.py
@@ -0,0 +1,13 @@
+import numpy as np
+import time
+
+
+def run(run_id, thetatr, ftr, x, method):
+    result = {}
+    return result
+
+if __name__ == "__main__":
+    paramval = np.loadtxt('testingdata/thetavals.csv',delimiter=',')
+    fval = np.loadtxt('testingdata/functionevals.csv',delimiter=',')
+    inputval = np.loadtxt('testingdata/inputdata.csv',delimiter=',',dtype='object')
+
