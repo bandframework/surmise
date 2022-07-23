fffc1bb4f7943c047019e76ace075616ffd5bc64
Moses Chan <mosesyhc@u.northwestern.edu>
2022-07-23 17:16:19 -0400

    update timing runs plot

--- research/emucomp/experiment code/plot_time.py
+++ research/emucomp/experiment code/plot_time.py
@@ -3,10 +3,10 @@ import pandas as pd
 import json
 import glob
 import seaborn as sns
-plt.style.use(['science', 'high-vis', 'grid'])
+plt.style.use(['science', 'no-latex', 'high-vis', 'grid'])
 
 
-parent_datadir = r'C:\Users\cmyh\Documents\git\surmise\research\emucomp\experiment code\emulator_timing_results\1\data'
+parent_datadir = r'C:\Users\moses\Desktop\git\surmise\research\emucomp\experiment code\emulator_timing_results\3\data'
 flist = glob.glob(parent_datadir + r'\*.json')
 d = []
 for fname in flist:
@@ -17,13 +17,15 @@ for fname in flist:
 root_df = pd.DataFrame(d)
 root_df['npts'] = root_df.n * root_df.nx * (1 - root_df.failfraction.mean())
 
-markers = ['D', 'v', 'X']
+markers = ['D', 'v', 'X', 'o']
 p = sns.scatterplot(x='npts', y='emutime',
-             hue='method',
-             style='method',
-             markers=markers,
-             s=80,
-             # lw=4,
-             data=root_df)
+               hue='method',
+               style='method',
+               markers=markers,
+               # logx=True,
+               s=80,
+               # lw=4,
+               data=root_df)
 p.set_xscale('log')
 p.set_yscale('log')
+
