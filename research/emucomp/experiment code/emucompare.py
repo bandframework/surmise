676c5989eaf4885e50f02b7b2400b7c1cd9e5bf4
Moses Chan <mosesyhc@u.northwestern.edu>
2022-08-02 09:46:20 -0500

    fix boolean argparse

--- research/emucomp/experiment code/emucompare.py
+++ research/emucomp/experiment code/emucompare.py
@@ -5,9 +5,17 @@ from testfunc_wrapper import TestFunc
 from emu_single_test import single_test
 parser = argparse.ArgumentParser(description='Takes argument to mainemutest().')
 
+def str2bool(s):
+    if s == 'True':
+        return True
+    elif s == 'False':
+        return False
+    else:
+        raise ValueError('Input should be either True or False')
+
 parser.add_argument('--n', type=int, help='number of parameters')
 parser.add_argument('--function', help='name of test function')
-parser.add_argument('--failrandom', type=bool, help='True if failures are random (False if structured)')
+parser.add_argument('--failrandom', type=str2bool, help='True if failures are random (False if structured)')
 parser.add_argument('--failfraction', type=float, help='fraction of failures')
 parser.add_argument('--method', help='name of emulator method')
 parser.add_argument('--rep', type=int, help='id of replication')
