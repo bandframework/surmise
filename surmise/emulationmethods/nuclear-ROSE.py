73c7d97032ea9c487e9fb736e2f9ccb28292602c
Moses Chan <mosesyhc@u.northwestern.edu>
2023-06-19 13:36:12 -0500

    a wrapper to nuclear-rose emulators

---
+++ surmise/emulationmethods/nuclear-ROSE.py
@@ -0,0 +1,66 @@
+"""nuclear-ROSE, an integration wrapper for using emulators produced by [fill-in] nuclear-rose package."""
+import numpy as np
+
+
+def fit(fitinfo, rose_emu, **kwargs):
+    '''
+    The purpose of fit is to take information and plug all of our fit
+    information into fitinfo, which is a python dictionary.
+
+    Parameters
+    ----------
+    fitinfo : dict
+        A dictionary including the emulation fitting information once
+        complete.
+        The dictionary is passed by reference, so it returns None.
+    args : dict, optional
+        A dictionary containing options. The default is None.
+
+    Returns
+    -------
+    None.
+
+    '''
+    fitinfo['emulator'] = rose_emu
+
+    return
+
+
+def predict(predinfo, fitinfo, x, theta, **kwargs):
+    '''
+    Parameters
+    ----------
+    predinfo : dict
+        An arbitary dictionary where you should place all of your prediction
+        information once complete.
+
+        - predinfo['mean'] : mean prediction.
+
+        - predinfo['cov'] : variance of the prediction.
+
+    x : numpy.ndarray
+        An array of inputs. Each row should correspond to a row in f.
+
+    theta : numpy.ndarray
+        An array of parameters. Each row should correspond to a column in f.
+
+    args : dict, optional
+        A dictionary containing options. The default is None.
+
+    Returns
+    -------
+    Prediction mean and variance at theta and x given the dictionary fitinfo.
+    '''
+
+    rose_emu = fitinfo['emulator']
+    outputArray = []
+
+    for i, thetai in enumerate(theta):
+        amplitudeEm = rose_emu.predict(thetai)
+        outputArray.append(10*np.real(np.conj(amplitudeEm[x])*amplitudeEm[x]))
+
+    predmean = np.array(outputArray).T.squeeze(0)
+
+    predinfo['mean'] = predmean
+    predinfo['var'] = np.zeros_like(predmean)
+    return
