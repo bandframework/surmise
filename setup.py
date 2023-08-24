da0a0273b841838add2306b8274387a942b6c649
Moses Chan <mosesyhc@u.northwestern.edu>
2023-08-23 22:00:00 -0500

    migrate from setup.py to toml

--- setup.py
+++ setup.py
@@ -20,30 +20,12 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 setup(
     name="surmise",
     version="0.1.1",
-    author="Matthew Plumlee, Özge Sürer, Stefan M. Wild",
-    author_email="ozgesurer2019@u.northwestern.edu",
-    description="A modular interface for surrogate models and tools",
-    url="https://github.com/surmising/surmise",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-    setup_requires=[
-        'setuptools>=18.0',
-        'cython'
-    ],
-    install_requires=[
-                      'numpy',
-                      'scipy'
-                      ],
-    extras_require={'extras': ['GPy'],
-                    'docs': ['sphinx', 'sphinxcontrib.bibtex', 'sphinx_rtd_theme']},
+    extras_require={'docs': ['sphinx', 'sphinxcontrib.bibtex', 'sphinx_rtd_theme']},
     cmdclass={'test': Run_TestSuite},
     ext_modules=[
-        Extension('surmise.emulationsupport.matern_covmat', sources=['surmise/emulationsupport/matern_covmat.pyx']),
+        Extension('surmise.emulationsupport.matern_covmat',
+                  sources=['surmise/emulationsupport/matern_covmat.pyx']),
     ],
     include_dirs=[numpy.get_include()]
 )
