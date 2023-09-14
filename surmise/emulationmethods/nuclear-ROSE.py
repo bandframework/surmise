b85bb640fd23b7d3156905c3a76fed1cadc7cef3
Moses Chan <mosesyhc@u.northwestern.edu>
2023-09-13 22:03:23 -0500

    nuclear rose adding user input emulator variance

--- surmise/emulationmethods/nuclear-ROSE.py
+++ surmise/emulationmethods/nuclear-ROSE.py
@@ -2,21 +2,26 @@
 import numpy as np
 
 
-def fit(fitinfo, rose_emu, **kwargs):
+def fit(fitinfo, rose_emu, emu_variance_constant=0.0, **kwargs):
     '''
     The purpose of fit is to take information and plug all of our fit
     information into fitinfo, which is a python dictionary.
 
     Example usage for ROSE:
     rbm = emulator(method='nuclear-ROSE',
-            args={'rose_emu': <<insert rose.ScatteringAmplitudeEmulator object>>})
+            args={'rose_emu': <<insert rose.ScatteringAmplitudeEmulator object>>,
+                  'emu_variance_constant': 1e-5})
 
     Parameters
     ----------
     fitinfo : dict
-        A dictionary including the emulation fitting information once
-        complete.
+        A dictionary including the emulation fitting information once complete.
         The dictionary is passed by reference, so it returns None.
+    rose_emu : nuclear-rose.ScatteringAmplitudeEmulator
+        ROSE emulator object.
+    emu_variance_constant : scalar, optional
+        User-defined emulator variance constant.  For example, one may estimate
+        this quantity empirically.  The default is 0.
     args : dict, optional
         A dictionary containing options. The default is None. Insert ROSE emulator with
         key = 'rose_emu'.
@@ -27,6 +32,7 @@ def fit(fitinfo, rose_emu, **kwargs):
 
     '''
     fitinfo['emulator'] = rose_emu
+    fitinfo['emulator_variance_constant'] = emu_variance_constant
 
     return
 
@@ -67,5 +73,5 @@ def predict(predinfo, fitinfo, x, theta, **kwargs):
     predmean = np.array(outputArray).T
 
     predinfo['mean'] = predmean
-    predinfo['var'] = np.zeros_like(predmean)
+    predinfo['var'] = np.ones_like(predmean) * fitinfo['emulator_variance_constant']
     return
