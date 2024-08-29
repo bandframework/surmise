5342e65b665e39324169beb2df4112751c430c3b
Jared O'Neal <joneal@anl.gov>
2024-08-29 13:17:12 -0500

    Simplify setup.py.
    See comments just added to that file.  This worked locally for tox builds with
    coverage.  Pushing to see if all GH actions pass.  Even so, we will have to see
    if these changes have a negative effect on building binary wheels.

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
-from setuptools import setup, Extension
-from setuptools.command.test import test as TestCommand
 import numpy
 
-
-class Run_TestSuite(TestCommand):
-    def run_tests(self):
-        import os
-        import sys
-        py_version = sys.version_info[0]
-        print('Python version from setup.py is', py_version)
-        run_string = "tests/run-tests.sh -p " + str(py_version)
-        os.system(run_string)
+from setuptools import setup
+from Cython.Build import cythonize
 
 
+# This project nominally configures package management via pyproject.toml.
+# However, that file is not setup to handle the Cython builds.  Therefore, we
+# presently include this call to ensure that the Cython build does proceed as
+# necessary.
 setup(
-    cmdclass={'test': Run_TestSuite},
-    ext_modules=[
-        Extension('surmise.emulationsupport.matern_covmat',
-                  sources=['surmise/emulationsupport/matern_covmat.pyx']),
-    ],
+    ext_modules=cythonize("surmise/emulationsupport/matern_covmat.pyx"),
     include_dirs=[numpy.get_include()]
 )
