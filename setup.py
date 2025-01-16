945edb794c069be240669d844491c096a828f880
Jared O'Neal <joneal@anl.gov>
2025-01-16 13:01:10 -0600

    (Issue #137) First try at separting pyproject.toml/setup.py cleanly.
    Put all information needed by pip to build distributions in a dedicated build
    system venv in the TOML file.  All information needed to dictate the composition
    of the package in setup.py.  I was able to remove dependencies.

--- setup.py
+++ setup.py
@@ -1,15 +1,79 @@
-import numpy
+# IMPORTANT
+# * Please adjust GitHub actions to match changes to Python version here.
+# * Please make sure that all dependence/version changes made here are reflected
+#   in the oldest tox task in tox.ini
 
+import numpy
+import codecs
+
+from pathlib import Path
 from setuptools import setup
 from Cython.Build import cythonize
 
+_PKG_ROOT = Path(__file__).resolve().parent
 
-# This project nominally configures package management via pyproject.toml.
-# However, that file is not setup to handle the Cython builds.  Therefore, we
-# presently include this call to ensure that the Cython build does proceed as
-# necessary.
+
+def readme_rst():
+    fname = _PKG_ROOT.joinpath("README.rst")
+    with codecs.open(fname, encoding="utf8") as fptr:
+        return fptr.read()
+
+
+# TODO: By this stage, it looks like the version should already be set in
+# _version.py by setuptools_scm as part of constructing the build system venv.
+# We aren't explicitly setting the version in setup.  Is it being set
+# automatically?  Do we need to set it?
+#def version():
+#    fname = _PKG_ROOT.joinpath("VERSION")
+#    with open(fname, "r") as fptr:
+#        return fptr.read().strip()
+
+python_requires = ">=3.9"
+code_requires = [
+    'numpy>=1.22.0',
+    'scipy>=1.9.0',
+    'dill>=0.3.8'
+]
+test_requires = []
+extras_require = {
+    "scikit-learn": ['scikit-learn>=1.2.0'],
+    "all": ['scikit-learn>=1.2.0']
+}
+install_requires = code_requires + test_requires
+
+package_data = {
+    "surmise": []
+}
+
+project_urls = {
+    "Source": "https://github.com/bandframework/surmise",
+    "Documentation": "https://surmise.readthedocs.io",
+    "Tracker": "https://github.com/bandframework/surmise/issues"
+}
+
 setup(
+    name="surmise",
+    author="Matthew Plumlee, Özge Sürer, Stefan M. Wild, and Moses Y.-H. Chan",
+    author_email="moses.chan@northwestern.edu",
+    maintainer="Moses Y.-H. Chan",
+    maintainer_email="moses.chan@northwestern.edu",
+    description="A modular interface for surrogate models and tools",
+    long_description=readme_rst(),
+    long_description_content_type="text/rst",
+    url="https://github.com/bandframework/surmise",
+    project_urls=project_urls,
+    license="MIT",
+    package_data=package_data,
+    python_requires=python_requires,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    include_dirs=[numpy.get_include()],
     ext_modules=cythonize("surmise/emulationsupport/matern_covmat.pyx",
-                          language_level = "3"),
-    include_dirs=[numpy.get_include()]
+                          language_level=3),
+    keywords="surmise",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
