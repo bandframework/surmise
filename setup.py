0338b60fcf6953d5f55785b9b01d0f658dd54764
Jared O'Neal <joneal@anl.gov>
2025-02-05 16:35:56 -0600

    (Issue #137) Try an even newer numpy version.

--- setup.py
+++ setup.py
@@ -30,8 +30,10 @@ def readme_rst():
 #        return fptr.read().strip()
 
 python_requires = ">=3.9"
+# numpy upper limits required by macos-13 GH action tests.  Without these,
+# there are segmentation faults.
 code_requires = [
-    'numpy>=1.22.0,<=2.1.0',
+    'numpy>=1.22.0,<=2.2.0',
     'scipy>=1.9.0,<=1.13.1',
     'dill>=0.3.8'
 ]
