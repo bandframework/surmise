2befeb9a432eaa196e21a6ff5b2146a5f4939092
Jared O'Neal <joneal@anl.gov>
2025-06-11 09:42:10 -0500

    (Issue #74) Expanding testing capabilities.
    If users want to run just a subset of the tests, they probably want to be in
    editable/developer mode.  This decision is likely only consequential in that it
    might make the tasks run a bit faster and if developers decide to activate one
    of the associated venvs instead of coverage or nocoverage.

---
+++ surmise/tests/conftest.py
@@ -0,0 +1,2 @@
+def pytest_addoption(parser):
+    parser.addoption("--cmdopt1", action="store", help="Name of an emulator")
--- surmise/tests/test_new_emu_uno.py
+++ surmise/tests/test_new_emu_uno.py
@@ -1,2 +1,18 @@
-def test_new_emu_uno():
-    assert 1 == 1
+import pytest
+
+
+@pytest.fixture
+def cmdopt1(request):
+    return request.config.getoption("--cmdopt1")
+
+# TODO: If I uncomment this, --cmdopt1 doesn't seem to have an effect.
+#@pytest.mark.parametrize(
+#    "cmdopt1", ["PCGP", "PCSK"]
+#)
+def test_new_emu_uno(cmdopt1):
+    if cmdopt1 == "PCGP":
+        assert 1 == 2
+    elif cmdopt1 == "PCSK":
+        assert 3 == 3
+    else:
+        assert "Please pass --cmdopt1=x" == ""
