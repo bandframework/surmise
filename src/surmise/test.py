2dc356e0dfc0182218ad1acbadf50d9f47b9e5aa
Jared O'Neal <joneal@anl.gov>
2025-06-25 12:23:51 -0500

    (Issue #74) Clean test.py as part of PR review.
    coverage & nocoverage tox tasks pass.  Ran surmise.test() directly in
    nocoverage venv.

--- src/surmise/test.py
+++ src/surmise/test.py
@@ -9,9 +9,9 @@ def test(level=0):
     """
     VERBOSITY = [0, 1, 2]
 
-    args = []
     if level not in VERBOSITY:
         raise ValueError(f"level must be in {VERBOSITY}")
-    args = [f"--verbosity={level}"]
 
-    return (pytest.main(args + ["--pyargs", "surmise.tests"]) == 0)
+    cmd = [f"--verbosity={level}", "--pyargs", "surmise.tests"]
+
+    return (pytest.main(cmd) == 0)
