dde7fc43d1340dafa3016bc2b001993db147ae73
Jared O'Neal <joneal@anl.gov>
2026-06-09 12:13:05 -0500

    (Issue #194) Cleaning as part of PR review.

--- tools/load_mcmc_results.py
+++ tools/load_mcmc_results.py
@@ -8,7 +8,8 @@ from pathlib import Path
 def load_mcmc_results(filename):
     GROUP = "/MCMC"
 
-    with h5py.File(filename, "r") as fptr:
+    fname = Path(filename).resolve()
+    with h5py.File(fname, "r") as fptr:
         method = fptr[GROUP].attrs["Method"]
         assert method == "Metropolis"
         acceptance_rate = fptr[GROUP].attrs["AcceptanceRate"]
