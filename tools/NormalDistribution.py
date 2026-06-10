651b96149a2e136f9664d09f20685cf78308a765
Jared O'Neal <joneal@anl.gov>
2026-06-10 09:24:55 -0500

    (Issue #194) Improve use of numpy arrays in 1D.
    This brings those classes into agreement with AbstractDistribution inline
    documentation contracts.

--- tools/NormalDistribution.py
+++ tools/NormalDistribution.py
@@ -33,15 +33,21 @@ class NormalDistribution(AbstractDistribution):
         return self.__N.stats("mv")
 
     def inv_cdf(self, p):
-        return self.__N.ppf(self._as2darray_checked(p))
+        values = self.__N.ppf(self._as1darray_checked(p))
+        assert values.ndim == 1
+        return values
 
     def pdf(self, theta):
-        return self.__N.pdf(self._as2darray_checked(theta))
+        values = self.__N.pdf(self._as1darray_checked(theta))
+        assert values.ndim == 1
+        return values
 
     def logpdf(self, theta, return_grad):
         if return_grad:
             raise NotImplementedError("gradient not implemented yet")
-        return self.__N.logpdf(self._as2darray_checked(theta))
+        values = self.__N.logpdf(self._as1darray_checked(theta))
+        assert values.ndim == 1
+        return values
 
     def sample(self, n, rng):
         samples = np.atleast_2d(self.__N.rvs(size=n, random_state=rng))
