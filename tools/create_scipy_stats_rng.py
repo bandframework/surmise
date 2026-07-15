1bd65111a4f0f64fad4f5aaf77fbe208708233f3
Jared O'Neal <joneal@anl.gov>
2026-07-15 13:56:34 -0500

    (Issue #188) Test MH with non-default bit generator type.
    As expected, the new results were not identical to the benchmark.  However, the
    new results look reasonable.

--- tools/create_scipy_stats_rng.py
+++ tools/create_scipy_stats_rng.py
@@ -4,9 +4,13 @@ import numpy as np
 def create_scipy_stats_rng(rng_cfg):
     rand_method = rng_cfg["method"]
     rand_seed = rng_cfg["random_seed"]
-    assert rand_method.lower() == "default"
 
     print(f"RNG method\t\t{rand_method}")
     print(f"Random seed\t\t{rand_seed}")
 
-    return np.random.default_rng(rand_seed)
+    if rand_method.lower() == "default":
+        return np.random.default_rng(rand_seed)
+    elif rand_method.upper() == "PCG64DXSM":
+        return np.random.Generator(np.random.PCG64DXSM(rand_seed))
+
+    raise ValueError(f"Unsupported bit generator ({rand_method})")
